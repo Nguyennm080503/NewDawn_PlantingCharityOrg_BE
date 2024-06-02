b576c8fb4c3d16004e01008adc5bffca2acbe577
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-06-02 19:01:25 +0700

    update code deploy

--- EXE201_NEWDAWN_BE/Controllers/NewsController/NewsController.cs
+++ EXE201_NEWDAWN_BE/Controllers/NewsController/NewsController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Service.Interface;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace EXE201_NEWDAWN_BE.Controllers.NewsController
 {
