501940f3b0a7ea8fd6a9cc1b28c9cf9d8053bc98
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-06-02 08:53:52 +0700

    update cors

--- EXE201_NEWDAWN_BE/EXE201_NEWDAWN_BE/Extensions/ApplicationServicesExtensions.cs
+++ EXE201_NEWDAWN_BE/EXE201_NEWDAWN_BE/Extensions/ApplicationServicesExtensions.cs
@@ -35,7 +35,7 @@ namespace EXE201_NEWDAWN_BE.Extensions
 
             services.AddCors(opt =>
             {
-                opt.AddPolicy("CorsPolicy", policy => { policy.WithOrigins("*").AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+                opt.AddPolicy("CorsPolicy", policy => { policy.WithOrigins("http://localhost:5173").AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
             });
 
 
